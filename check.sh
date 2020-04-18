fade61a8339cd51952c6d10e26a0288a49b2db9f
Martin Chadderton <mvchad@yahoo.co.uk>
2020-04-18 20:22:25 +0100

    Add files via upload

--- check.sh
+++ check.sh
@@ -75,6 +75,7 @@ github "jquery/jquerymobile.com" "jquery-mobile"
 xml "https://raw.githubusercontent.com/zaproxy/zap-admin/master/ZapVersions.xml" "/ZAP/core/version/text()" "OWASP ZAP"
 github "OWASP/Amass" "OWASP Amass"
 github "virustotal/yara" "YARA"
+github "java-decompiler/jd-gui" "jd-gui"
 atom "http://hg.nginx.org/nginx/atom-log" "nginx"
 tomcat 9
 linux
