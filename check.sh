bc4258ea5f9a834a785194e00e138ffe263f2586
Martin Chadderton <mvchad@yahoo.co.uk>
2020-04-19 14:47:17 +0100

    Slight restructure + add OWASP dependency check

--- check.sh
+++ check.sh
@@ -2,19 +2,20 @@ function title() {
   printf "%-20s: " "${1}"
 }
 
+function maintitle() {
+  echo "**** ${1} ****"
+}
+
 function xml() {
   # $1 - URL
   # $2 - xpath
   # $3 - echo string
   title "${3}"
-  #echo -n "${3}="
   curl -s "${1}" 2>&1 | xmllint --xpath "${2}" -
 }
 
 function linux() { 
-  echo ""
-  echo "Linux Kernel Releases"
-  echo "*********************"
+  maintitle "Linux Kernel Releases"
   curl -s "https://www.kernel.org/feeds/kdist.xml" 2>&1 | xmllint --xpath "//item/title/text()" -
 }
 
@@ -26,7 +27,8 @@ function html() {
   curl -s "${1}" 2>&1 | tidy -q --show-warnings no | xmllint --html --xpath "${2}" 2>&1 -  
 }
 
-function ords() {
+function oracle() {
+  maintitle "ORACLE"
   title "ORDS"
   curl -s "https://www.oracle.com/database/technologies/appdev/rest-data-services-downloads.html" 2>&1 | \
     tidy -q --show-warnings no | \
@@ -39,7 +41,7 @@ function atom() {
   title "${2}"
   curl -s "${1}" 2>&1 | \
       xmllint --xpath "//*[local-name()='feed']/*[local-name()='entry']/*[local-name()=\"title\"]" --xmlout - | \
-      tidy -q --show-warnings no | \
+      tidy -q --show-warnings no  -w 100000 | \
 	  xmllint --html --xpath "//title[1]/text()" - 
 }
 
@@ -58,26 +60,52 @@ function sqlite() {
 }
 
 function tomcat() {
-title "Tomcat ${1}"
+title "tomcat ${1}"
 curl -s "http://mirror.vorboss.net/apache/tomcat/tomcat-${1}/?C=M;O=D" 2>&1 | \
      tidy -q --show-warnings no | \
 	 xmllint --html --xpath "string(/html/body//a[starts-with(@href,\"v9.0\")][1]/@href)" - | \
 	 cut -d '/' -f 1
 }
 
-ords
-sqlite
-html "https://mirrors.ukfast.co.uk/sites/ftp.apache.org/jmeter/binaries/" "string(/html/body/pre/a[3]/@href)" "jmeter"
-html "http://mirror.vorboss.net/apache/httpd/" "string(//a[contains(@href, \".tar.gz\")]/@href)" "httpd"
-github "jquery/jquery" "jquery"
-github "jquery/jquery-ui" "jquery-ui"
-github "jquery/jquerymobile.com" "jquery-mobile"
-xml "https://raw.githubusercontent.com/zaproxy/zap-admin/master/ZapVersions.xml" "/ZAP/core/version/text()" "OWASP ZAP"
-github "OWASP/Amass" "OWASP Amass"
-github "virustotal/yara" "YARA"
-github "java-decompiler/jd-gui" "jd-gui"
-github "aircrack-ng/aircrack-ng" "aircrack-ng"
-github "openssl/openssl" "OpenSSL"
-atom "http://hg.nginx.org/nginx/atom-log" "nginx"
-tomcat 9
+function owasp() {
+  maintitle "OWASP"
+  xml "https://raw.githubusercontent.com/zaproxy/zap-admin/master/ZapVersions.xml" "/ZAP/core/version/text()" "ZAP"
+  github "OWASP/Amass" "Amass"
+  github "jeremylong/DependencyCheck" "Dependency-Check"
+}
+
+function jquery() {
+  maintitle "JQUERY"
+  github "jquery/jquery" "jquery"
+  github "jquery/jquery-ui" "jquery-ui"
+  github "jquery/jquerymobile.com" "jquery-mobile"
+}
+
+function apache() {
+  maintitle "APACHE"
+  html "https://mirrors.ukfast.co.uk/sites/ftp.apache.org/jmeter/binaries/" "string(/html/body/pre/a[3]/@href)" "jmeter"
+  html "http://mirror.vorboss.net/apache/httpd/" "string(//a[contains(@href, \".tar.gz\")]/@href)" "httpd"
+  tomcat 9
+}
+
+function nginx() {
+  maintitle "NGINX"
+  atom "http://hg.nginx.org/nginx/atom-log" "nginx"
+}
+
+function misc() {
+  maintitle "MISC"
+  sqlite
+  github "virustotal/yara" "YARA"
+  github "java-decompiler/jd-gui" "jd-gui"
+  github "aircrack-ng/aircrack-ng" "aircrack-ng"
+  github "openssl/openssl" "OpenSSL"
+}
+
+oracle
+nginx
+apache
+jquery
+owasp
+misc
 linux
