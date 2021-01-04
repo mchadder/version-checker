d34d920b9a8b944807169ee880f92da3827fb797
Martin Chadderton <mvchad@yahoo.co.uk>
2021-01-04 16:26:15 +0000

    Add files via upload
    Major changes to read github API creds from creds.config and add/change new repositories

--- check.sh
+++ check.sh
@@ -17,8 +17,47 @@ function title() {
   printf "%-20s: " "${1}"
 }
 
+function github_entry() {
+  # entry <title> <date> <latest version> <latest tag>
+  printf "%-27s: %-37s: %s (%s)" "${MAGENTA}${1}" "${YELLOW}${2}" "${GREEN}${3:0:60}" "${4:0:30}"
+  echo -e "${NORMAL}"
+}
+
+function github() {
+  # $1 - URL
+  # $2 - echo string
+  # $3 - use github api? (N)
+
+  source creds.config 
+
+  GITHUB_RELEASES_URL="https://api.github.com/repos/${1}/releases/latest"
+  GITHUB_TAGS_URL="https://api.github.com/repos/${1}/tags"
+
+  # Only use the GITHUB api if specified to do so
+  if [ "${3}" = "Y" ]
+  then
+    curl -s -u "${GITHUB_USERNAME}:${GITHUB_TOKEN}" -o github_releases.json "${GITHUB_RELEASES_URL}"
+
+    LATEST_RELEASE_VERSION=$(jq -r ".tag_name" github_releases.json)
+    LATEST_RELEASE_DATE=$(jq -r ".created_at" github_releases.json)
+
+    curl -s -u "${GITHUB_USERNAME}:${GITHUB_TOKEN}" -o github_tags.json "${GITHUB_TAGS_URL}"
+
+    # Unfortunately, github do not have a "get the latest tag" (even though tags have timestamps!)
+    # Tag names in github follow the semver rules (https://semver.org/)
+    # TODO: This should omit all "non-semver" tags really
+    LATEST_TAG=$(jq -r ".[0].name" github_tags.json)
+
+    #rm github_releases.json github_tags.json
+
+    github_entry "$2" "${LATEST_RELEASE_DATE}" "${LATEST_RELEASE_VERSION}" "${LATEST_TAG}"
+  else
+    atom "https://github.com/${1}/releases.atom" "${2}"
+  fi
+}
+
 function entry() {
-  # entry <title> <date> <version>
+  # entry <title> <date> <latest version>
   printf "%-27s: %-37s: %s" "${MAGENTA}${1}" "${YELLOW}${2}" "${GREEN}${3:0:60}"
   echo -e "${NORMAL}"
 }
@@ -61,7 +100,6 @@ function atom() {
 }
 
 function linux() { 
-  maintitle "Linux Kernel Releases"
   curl -s "https://www.kernel.org/feeds/kdist.xml" 2>&1 | xmllint --xpath "//item/title/text()" -
 }
 
@@ -72,19 +110,7 @@ function html() {
   entry "${3}" "" $(curl -s "${1}" 2>&1 | tidy -q --show-warnings no | xmllint --html --xpath "${2}" 2>&1 -)
 }
 
-function java() {
-  # linux-x64.tar.gz
-  maintitle "JAVA"
-
-  title "Oracle Java 8"
-  curl -s "https://www.oracle.com/java/technologies/javase-jre8-downloads.html" 2>&1 | \
-    tidy -q --show-warnings no | \
-    xmllint --html --xpath "string(//a[contains(@href,\".zip\")]/text())" - 2>&1
-}
-
 function oracle() {
-  maintitle "ORACLE"
-
   github "utPLSQL/utPLSQL-SQLDeveloper" "utPLSQL SQLDeveloper"
   github "utPLSQL/utPLSQL" "utPLSQL"
   github "Trivadis/plsql-unwrapper-sqldev" "PL/SQL Unwrapper"
@@ -98,12 +124,6 @@ function oracle() {
   rss "https://www.oracle.com/ocom/groups/public/@otn/documents/webcontent/rss-otn-sec.xml" "CPU"
 }
 
-function github() {
-  # $1 - URL
-  # $2 - echo string
-  atom "https://github.com/${1}/releases.atom" "${2}"
-}
-
 function sqlite() {
   OUTPUT=$(curl -s "https://www.sqlite.org/chronology.html")
   entry "sqlite" "" $(echo -n ${OUTPUT} | \
@@ -112,27 +132,43 @@ function sqlite() {
 	xmllint --html -xpath "//a[1]/text()" -)
 }
 
+function httpd() {
+  html "http://mirror.vorboss.net/apache/httpd/" "string(//a[contains(@href, \".tar.gz\")]/@href)" "httpd"
+}
+
 function tomcat() {
   OUTPUT=$(curl -s "http://mirror.vorboss.net/apache/tomcat/tomcat-${1}/?C=M;O=D" 2>&1)
   entry "tomcat ${1}" "" $(echo -n ${OUTPUT} | \
      tidy -q --show-warnings no | \
-	 xmllint --html --xpath "string(/html/body//a[starts-with(@href,\"v${1}\")][1]/@href)" - | \
+	 xmllint --html --xpath "string(/html/body//a[starts-with(@href,\"v${1}\")][last()]/@href)" - | \
 	 cut -d '/' -f 1)
 }
 
 function fuse() {
-  maintitle "FUSE FS"
   github "cryfs/cryfs" "cryfs"
   github "libfuse/sshfs" "sshfs"
   github "rfjakob/gocryptfs" "gocryptfs"
   github "thkala/fuseflt" "fuseflt"
 }
 
+function owasp() {
+  xml "https://raw.githubusercontent.com/zaproxy/zap-admin/master/ZapVersions.xml" "/ZAP/core/version/text()" "OWASP ZAP"
+  github "OWASP/Amass" "OWASP Amass"
+  github "coreruleset/coreruleset" "OWASP Core Rule Set"
+  github "OWASP/ASVS" "OWASP ASVS"
+  github "OWASP/owasp-masvs" "OWASP MASVS" "Y"
+  github "OWASP/wstg" "OWASP WSTG"
+  github "jeremylong/DependencyCheck" "OWASP Dependency Check"
+  github "DependencyTrack/dependency-track" "OWASP Dependency Track"
+}
+
 function pentest() {
-  maintitle "PENTESTING"
   github "NationalSecurityAgency/ghidra" "NSA Ghidra"
   github "virustotal/yara" "YARA"
   github "OSUSecLab/InputScope" "InputScope"
+  github "libinjection/libinjection" "libinjection"
+  github "SpiderLabs/ModSecurity" "ModSecurity"
+  github "SpiderLabs/ModSecurity-nginx" "ModSecurity-nginx"
   github "java-decompiler/jd-gui" "jd-gui"
   github "aircrack-ng/aircrack-ng" "aircrack-ng"
   github "skylot/jadx" "jadx"
@@ -141,37 +177,17 @@ function pentest() {
   github "apache/jmeter" "Apache Jmeter"  
   github "cloudlinux/kcare-uchecker" "Uchecker"
   github "insidersec/insider" "Insider"
+  github "pmd/pmd" "PMD"
   rss "https://portswigger.net/burp/releases/rss" "BURP Suite"
 
   # IDS
   github "snort3/snort3" "Snort 3"
 
-  # OWASP tools
-  xml "https://raw.githubusercontent.com/zaproxy/zap-admin/master/ZapVersions.xml" "/ZAP/core/version/text()" "OWASP ZAP"
-  github "OWASP/Amass" "OWASP Amass"
-  github "SpiderLabs/owasp-modsecurity-crs" "OWASP Core Rule Set"
-  github "OWASP/ASVS" "OWASP ASVS"
-  github "OWASP/wstg" "OWASP WSTG"
-  github "jeremylong/DependencyCheck" "OWASP Dependency Check"
-  github "DependencyTrack/dependency-track" "OWASP Dependency Track"
-}
-
-function js() {
-  maintitle "JS FRAMEWORKS"
-  github "denoland/deno" "Deno"
-  github "jquery/jquery" "jquery"
-  github "jquery/jquery-ui" "jquery-ui"
-  github "jquery/jquerymobile.com" "jquery-mobile"
-  github "twbs/bootstrap" "Bootstrap"
-  github "handlebars-lang/handlebars.js" "Handlebars"
-  github "requirejs/requirejs" "Require"
-  github "select2/select2" "Select2"
-  github "jsplumb/jsPlumb" "jsplumb"
+  owasp
 }
 
 function python() {
-  maintitle "Python"
-  github "Legrandin/pycryptodome" "pycryptodome"
+  github "Legrandin/pycryptodome" "pycryptodome" "Y"
   github "Demonware/jose" "jose"
   github "jpadilla/pyjwt" "pyjwt"
   github "kivy/buildozer" "Buildozer"
@@ -180,32 +196,74 @@ function python() {
   github "psf/requests-html" "PSF Requests-HTML"
   github "datastax/python-driver" "cassandra-driver"
   github "pinterest/pymemcache" "pymemcache"
+  github "ytdl-org/youtube-dl" "youtube-dl"
+  github "aio-libs/aiohttp" "aiohttp"
+  github "certifi/python-certifi" "certifi"
+}
+
+function nps() {
+  github "jquery/jquery" "jquery"
+  github "jquery/jquery-ui" "jquery-ui"
+  github "jquery/jquerymobile.com" "jquery-mobile"
+  github "jquery/jquery-mousewheel" "jquery-mousewheel"
+
+  # This project is not maintained and superceded by js-cookie/js-cookie
+  # but this is used in s4atb
+  github "carhartl/jquery-cookie" "jquery-cookie"
+
+  github "hammerjs/hammer.js" "hammer"
+  github "twbs/bootstrap" "Bootstrap" "Y"
+  github "handlebars-lang/handlebars.js" "Handlebars"
+  github "requirejs/requirejs" "Require"
+  github "requirejs/almond" "Almond"
+  github "requirejs/i18n" "Require i18n"
+  github "select2/select2" "Select2" "Y"
+  github "jsplumb/jsPlumb" "jsplumb"
+  github "davidshimjs/qrcodejs" "qrcode.js" "Y"
+  github "jeffreykemp/jk64-plugin-simplemap" "Simple Google Map"
+  github "RonnyWeiss/Apex-Fancy-Tree-Select" "Apex Fancy Tree"
+  github "antonscheffer/excel2collections" "Excel2Collection"
+  github "Dani3lSun/apex-plugin-timeline" "Timeline"
+  github "Pretius/apex-nested-reports" "Nested Reports" "Y"
+  github "glebovpavel/IR_to_MSExcel" "IR to MSExcel"
+  github "guillaumepotier/Parsley.js" "Parsley JS"
+  oracle
+  tomcat 9
+  github "eclipse/jetty.project" "Eclipse Jetty"
+  github "curl/curl" "curl"
+  github "swagger-api/swagger-editor" "Swagger Editor"
 }
 
 function misc() {
-  maintitle "MISC"
   sqlite
+  github "denoland/deno" "Deno"
   github "pivpn/pivpn" "PiVPN"
   github "openssl/openssl" "OpenSSL"  
   github "jenkinsci/jenkins" "Jenkins"
   github "swagger-api/swagger-editor" "Swagger Editor"
   github "swagger-api/swagger-ui" "Swagger UI"
   github "Kong/kong" "Kong"
+  github "hockeypuck/hockeypuck" "HockeyPuck (HKP)"
   github "intel/Intel-Linux-Processor-Microcode-Data-Files" "Intel ucode"
-  github "RetroPie/RetroPie-Setup" "RetroPie"
+}
+
+function emu() {
+  github "midwan/amiberry" "Amiberry"
+  github "RetroPie/RetroPie-Setup" "RetroPie" "Y"
 }
 
 function http() {
-  maintitle "HTTP"
+#  maintitle "HTTP"
   github "allinurl/goaccess" "goaccess"
   github "curl/curl" "curl"
-  html "http://mirror.vorboss.net/apache/httpd/" "string(//a[contains(@href, \".tar.gz\")]/@href)" "httpd"
+  httpd
   tomcat 8
   tomcat 9
   tomcat 10
   atom "http://hg.nginx.org/nginx/atom-tags" "nginx"
   github "nginx/unit" "nginx unit"  
   github "openresty/openresty" "openresty"
+  github "eclipse/jetty.project" "Eclipse Jetty"
 }
 
 case "$SECTION" in
@@ -218,11 +276,11 @@ case "$SECTION" in
   PYTHON)
     python
     ;;
+  OWASP)
+    owasp
+    ;;
   PENTEST)
     pentest
-    ;;
-  JS)
-    js
     ;;
   FUSE)
     fuse 
@@ -233,14 +291,28 @@ case "$SECTION" in
   LINUX)
     linux
     ;;
+  NPS)
+    nps
+    ;;
+  EMU)
+    emu
+    ;;
   *)
+    maintitle "ORACLE"
     oracle
+    maintitle "HTTP"
     http
+    maintitle "PYTHON"
     python 
-    js
+    maintitle "FUSE FS"
     fuse
+    maintitle "PENTESTING"
     pentest
+    maintitle "MISC"
     misc
+    maintitle "EMU"
+    emu
+    maintitle "Linux Kernel Releases"
     linux
     ;;
 esac
